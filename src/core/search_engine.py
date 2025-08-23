8b7c614b650d5497430978c252061fdf040ebd53
Nico Rossi <nrossi@itba.edu.ar>
2025-08-22 23:24:51 -0300

    Update engine to handle cost-based search algo

--- src/core/search_engine.py
+++ src/core/search_engine.py
@@ -1,5 +1,5 @@
 import time
-from typing import Optional, List, Set, Tuple
+from typing import Optional, List, Set, Tuple, Dict, Callable
 from src.core.state import SokobanState, StateNode
 from src.core.result import SearchResult
 from src.core.interfaces import ISearchAlgorithm, IHeuristic
@@ -29,36 +29,77 @@ class SearchEngine:
 
     def search(self, initial_state: SokobanState) -> SearchResult:
         self._init_metrics()
-        current_node = StateNode(initial_state, parent=None, action="START")
-
+        start_node = StateNode(initial_state, parent=None, action="START")
         if initial_state.is_goal():
-            states, actions = _reconstruct_path_from_node(current_node)
-            return self._create_success(states, actions)
+            return self._create_success_from_node(start_node)
 
-        closed_states: Set[SokobanState] = set()
-        closed_states.add(current_node.state)
+        self.algorithm.add(start_node, self.heuristic)
+        self._update_frontier_size()
 
-        self.algorithm.add(current_node, self.heuristic)
-        self.max_frontier_size = max(self.max_frontier_size, self.algorithm.size())
+        if self.algorithm.cache_cost():
+            return self._search_with_cost_caching(initial_state)
+        else:
+            return self._search_without_cost_caching(initial_state)
 
-        # Core search loop driven by the algorithm implementation.
+    def _search_with_cost_caching(self, initial_state: SokobanState) -> SearchResult:
+        best_costs: Dict[SokobanState, int] = {initial_state: 0}
+        closed_set: Set[SokobanState] = set()
+
         while self.algorithm.has_next():
             current_node = self.algorithm.get_next()
+            if current_node.cost > best_costs.get(current_node.state, float("inf")):
+                continue
+
+            closed_set.add(current_node.state)
+            if current_node.state.is_goal():
+                return self._create_success_from_node(current_node)
+
             self.nodes_expanded += 1
 
             for successor_node in current_node.get_successors():
-                if successor_node.state in closed_states:
+                state = successor_node.state
+                g = successor_node.cost
+                if g >= best_costs.get(state, float("inf")):
                     continue
-                closed_states.add(successor_node.state)
 
-                if successor_node.state.is_goal():
-                    states, actions = _reconstruct_path_from_node(successor_node)
-                    return self._create_success(states, actions)
+                best_costs[state] = g
+                successor_node.parent = current_node
+                if state in closed_set:
+                    closed_set.remove(state)
 
                 self.algorithm.add(successor_node, self.heuristic)
-                self.max_frontier_size = max(self.max_frontier_size, self.algorithm.size())
+                self._update_frontier_size()
 
         return self._create_failure()
+
+    def _search_without_cost_caching(self, initial_state: SokobanState) -> SearchResult:
+        closed_states: Set[SokobanState] = {initial_state}
+
+        while self.algorithm.has_next():
+            current_node = self.algorithm.get_next()
+            if current_node.state.is_goal():
+                return self._create_success_from_node(current_node)
+
+            self.nodes_expanded += 1
+
+            for successor_node in current_node.get_successors():
+                state = successor_node.state
+                if state in closed_states:
+                    continue
+
+                closed_states.add(state)
+                successor_node.parent = current_node
+                self.algorithm.add(successor_node, self.heuristic)
+                self._update_frontier_size()
+
+        return self._create_failure()
+
+    def _update_frontier_size(self) -> None:
+        self.max_frontier_size = max(self.max_frontier_size, self.algorithm.size())
+
+    def _create_success_from_node(self, node: StateNode) -> SearchResult:
+        states, actions = _reconstruct_path_from_node(node)
+        return self._create_success(states, actions)
 
     def _init_metrics(self) -> None:
         self.start_time = time.time()
